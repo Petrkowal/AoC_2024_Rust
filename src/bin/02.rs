b5ea8d1f444a1b36cfeaab5362c4a1cc78328c53
Petrkowal <petr.krautwurst@seznam.cz>
2024-12-02 20:38:02 +0100

    Day 02 - Shortened pt2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,9 +1,10 @@
+use adv_code_2024::*;
 use anyhow::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use adv_code_2024::*;
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 const DAY: &str = "02";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -36,9 +37,9 @@ const TEST: &str = "\
 // Analyze the unusual data from the engineers. How many reports are safe?
 
 /// Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
-// 
+//
 // More of the above example's reports are now safe:
-// 
+//
 // 7 6 4 2 1: Safe without removing any level.
 // 1 2 7 8 9: Unsafe regardless of which level is removed.
 // 9 7 6 2 1: Unsafe regardless of which level is removed.
@@ -46,7 +47,7 @@ const TEST: &str = "\
 // 8 6 4 4 1: Safe by removing the third level, 4.
 // 1 3 6 7 9: Safe without removing any level.
 // Thanks to the Problem Dampener, 4 reports are actually safe!
-// 
+//
 // Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
 
 fn main() -> Result<()> {
@@ -56,7 +57,15 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        Ok(reader.lines().filter_map(Result::ok).map(|line| line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>()).filter(|levels| levels.windows(2).all(|w| (w[0] - w[1]).abs() <= 3 && w[0] != w[1]) && (levels.windows(2).all(|w| w[0] < w[1]) || levels.windows(2).all(|w| w[0] > w[1]))).count())
+        Ok(reader
+            .lines().filter_map(Result::ok).map(|line|
+                line.split_whitespace()
+                    .map(|s| s.parse::<i32>().unwrap())
+                    .collect::<Vec<_>>()
+            ).filter(|levels|
+                levels.windows(2).all(|w| w[0] < w[1] && w[1] - w[0] <= 3)
+                    || levels.windows(2).all(|w| w[0] > w[1] && w[0] - w[1] <= 3)
+            ).count())
     }
 
     assert_eq!(2, part1(BufReader::new(TEST.as_bytes()))?);
@@ -70,57 +79,20 @@ fn main() -> Result<()> {
     println!("\n=== Part 2 ===");
 
     fn part2<R: BufRead>(reader: R) -> Result<usize> {
-        let mut count: usize = 0;
-        for line in reader.lines() {
-            let line = line?;
-            let mut parts = line.split_whitespace();
-            let mut levels: Vec<i32> = Vec::new();
-            while let Some(part) = parts.next() {
-                levels.push(part.parse().unwrap());
-            }
-            print!("{:?}", levels);
-        
-            let mut any_safe = false;
-            for j in 0..levels.len() {
-                let mut increasing = false;
-                let mut decreasing = false;
-                let mut is_unsafe = false;
-        
-                for i in 0..levels.len() - 1 {
-                    if i == j {
-                        continue;
-                    }
-                    let mut plus_one = 0;
-                    if i + 1 == j {
-                        plus_one = 1;
-                    }
-                    if (i + 1 + plus_one) >= levels.len() {
-                        continue;
-                    }
-                    if levels[i] < levels[i + 1 + plus_one] {
-                        increasing = true;
-                    } else if levels[i] > levels[i + 1 + plus_one] {
-                        decreasing = true;
-                    }
-                    if (levels[i] - levels[i + 1 + plus_one]).abs() > 3 || levels[i] == levels[i + 1 + plus_one] {
-                        is_unsafe = true;
-                    }
-                }
-                if increasing && decreasing {
-                    is_unsafe = true;
-                }
-                if !is_unsafe {
-                    any_safe = true;
-                    break;
-                }
-            }
-            if any_safe {
-                count += 1;
-            }
-            println!(" is {}safe", if any_safe { "" } else { "un" });
-        }
-        
-        Ok(count)
+        Ok(reader
+            .lines()
+            .filter_map(Result::ok)
+            .map(|line|
+                line.split_whitespace()
+                    .map(|s| s.parse::<i32>().unwrap())
+                    .collect::<Vec<_>>())
+            .filter(|levels|
+                levels.iter().enumerate().any(|(i, _)| { // Maybe... Find a way to oneliner this later
+                    let mut levels = levels.clone();
+                    levels.remove(i);
+                    levels.windows(2).all(|w| w[0] < w[1] && w[1] - w[0] <= 3)
+                        || levels.windows(2).all(|w| w[0] > w[1] && w[0] - w[1] <= 3)
+                })).count())
     }
 
     assert_eq!(4, part2(BufReader::new(TEST.as_bytes()))?);
